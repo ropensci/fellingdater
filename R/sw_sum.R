724ddddc27fae1df63d18e05291a3cc9c4b5cd8e
hanecakr <Kristof.Haneca@vlaanderen.be>
2023-03-13 10:46:25 +0100

    fixing errors in sw_sum_plot

--- R/sw_sum.R
+++ R/sw_sum.R
@@ -1,19 +1,40 @@
-#' sw_sum
+#' sw_sum: summed probabilities of felling date ranges.
 #'
-#' @param x data.frame containing columns c("series", "swr", "waneyedge" , "last"), Column "sw_model" is optional. Could be the output of read_fh(x, header = TRUE)
-#' @param scale_p logical, TRUE or FALSE
-#' @param series name of the column containing the id's
-#' @param last name of the column containing the calendar year assigned to the last measured ring. Should be numeric
-#' @param swr name of the column containing the number of sapwood rings. Should be numeric
-#' @param waneyedge ame of the column indicating the presence (TRUE)/absence (FALSE) of waney edge. Should be logical
-#' @param sw_model apwood data set to be used. Should be one of sw_data_overview()
-#' @param credMass scalar [0, 1], specifying the mass within the credible interval (default = .954)
+#' @param x data.frame containing columns c("series", "swr", "waneyedge" , "last"),
+#'  Column "sw_model" is optional. Could be the output of read_fh(x, header = TRUE)
+#' @param series Name of the column containing the id's
+#' @param last Name of the column containing the calendar year assigned to the last measured ring. Should be numeric.
+#' @param n_sapwood Name of the column in `x` where the number of observed
+#'  sapwood rings are listed. This variable should be `numeric`.
+#' @param waneyedge Name of the column indicating the presence (TRUE)/absence (FALSE) of waney edge. Should be logical
+#' @param credMass A `scalar [0, 1]` specifying the mass within the credible
+#'  interval (default = .954).
+#' @param sw_data The name of the sapwood data set to use for modelling.
+#'  Should be one of [sw_data_overview()], or the path to a .csv file with
+#'  columns ´n_sapwood´ and ´count´.
+#' @param densfun Name of the density function fitted to the sapwood data set.
+#'   Should be one of:
+#'   * "lognormal" (the default value),
+#'   * "normal",
+#'   * "weibull",
+#'   * "gammma".
 #' @param plot logical, indicates whether a plot of the summed probability  distribution (SPD) should be returned.
+#' @param scale_p logical, TRUE or FALSE.
 #'
 #' @description Computes the summed probability density (SPD) for a set of felling date ranges.
 #'
 #' @return a data.frame
 #' @export
+#' @examples
+#' dummy7 <- data.frame(
+#' series = c("trs_40", "trs_41", "trs_42", "trs_43", "trs_44", "trs_45",
+#'  "trs_46", "trs_47", "trs_48"),
+#' last = c(1000, 1009, 1007, 1007, 1010, 1020, 1025, 1050, 1035),
+#' n_sapwood = c(5, 10, 15, 16, 8, 0, 10, NA, 1),
+#' waneyedge = c(FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE)
+#' )
+#'
+#' sw_sum(dummy7, plot = TRUE)
 #'
 sw_sum <- function (x,
                 series = "series",
@@ -26,6 +47,7 @@ sw_sum <- function (x,
                 plot = FALSE,
                 scale_p = FALSE){
 
+
         df <- as.data.frame(x)
 
         cambium <- df[[waneyedge]]
@@ -35,21 +57,24 @@ sw_sum <- function (x,
                 cambium <- ifelse(grepl("wk", cambium, ignore.case = TRUE), TRUE, FALSE)
         }
 
-        swr <- df[, n_sapwood]
+        swr <- df[[n_sapwood]]
         if (is.character(swr)) {
                 stop("--> 'n_sapwood' must be a numeric vector")
         }
 
-        if (any(is.na(swr))) {
-                which.nna <- which(is.na(swr) & !cambium)
+        which.nna <- which(is.na(swr) & !cambium)
+        if (length(which.nna) > 0) {
                 df <- df[-which.nna, ]
-                swr <- df[, n_sapwood]
+                swr <- df[[n_sapwood]]
+                cambium <- df[[waneyedge]]
 
-                warning(paste0(" --> ", length(which.nna), " Series without sapwood rings or waney edge detected.
-                        Those series are removed from the data set")
+                warning(paste0(" --> ", length(which.nna), " Series without sapwood rings or waney edge detected\n and removed from the data set")
                 )
         }
-        cambium <- df[[waneyedge]]
+
+        if (nrow(df) == 0) {
+                stop("no series with sapwood or waney edge in dataset")
+        }
 
         keycodes <- df[[series]]
         keycodes <- as.character(keycodes)
@@ -57,7 +82,7 @@ sw_sum <- function (x,
                 stop("--> some 'series' have no id")
         }
 
-        endDate <- df[, last]
+        endDate <- df[[last]]
         if (!is.numeric(endDate)) {
                 stop(" --> 'last' must be a numeric vector")
         }
@@ -113,19 +138,24 @@ sw_sum <- function (x,
      }
 
      # sum probabilities to SPD
-     if (length(wk.true > 0)) {
-     tmp <- pdf_matrix[, c("year", wk.true)]
-     if (dim(tmp)[2] > 2) tmp$SPD_wk <- rowSums(tmp[, -1], na.rm = TRUE)
-     if (ncol(tmp) == 2) tmp$SPD_wk <- tmp[, 2]
-     if (ncol(tmp) == 1) tmp$SPD_wk <- NA
-     } else { pdf_matrix$SPD_wk <- NA}
+     if (length(wk.true) == 0) {
+             tmp <- pdf_matrix[, "year", drop = FALSE ]
+             tmp$SPD_wk <- 0
 
-     tmp <- tmp[, c("year", "SPD_wk")]
-     tmp2 <- pdf_matrix[, setdiff(names(pdf_matrix), wk.true)]
+     } else {
+             tmp <- pdf_matrix[, c("year", wk.true), drop = FALSE]
+             if (dim(tmp)[2] > 2) tmp$SPD_wk <- rowSums(tmp[, -1], na.rm = TRUE)
+             if (ncol(tmp) == 2) tmp$SPD_wk <- tmp[, 2]
+             if (ncol(tmp) == 1) tmp$SPD_wk <- 0
+             tmp <- tmp[, c("year", "SPD_wk")]
+
+     }
+
+tmp2 <- pdf_matrix[, setdiff(names(pdf_matrix), wk.true), drop = FALSE]
 
      if (dim(tmp2)[2] > 2) pdf_matrix$SPD <- rowSums(tmp2[, -1], na.rm = TRUE)
      if (dim(tmp2)[2] == 2) pdf_matrix$SPD <- tmp2[, 2]
-     if (dim(tmp2)[2] == 1) pdf_matrix$SPD <- NA
+     if (dim(tmp2)[2] < 2) pdf_matrix$SPD <- 0
 
      pdf_matrix <- merge(pdf_matrix, tmp, by = "year", all = TRUE)
 
--- R/sw_sum_plot.r
+++ R/sw_sum_plot.r
@@ -1,4 +1,4 @@
-#' Tsw_sum_plot
+#' sw_sum_plot: plot the output of `sw_sum()`
 #'
 #' @description This function plots the result of `sw_sum()` - both the SPD and
 #'   the occurrence of exact felling dates - and adds a smoothing spline to the SPD.
@@ -11,12 +11,24 @@
 #' @export
 #'
 #' @examples
-#' sw_sum(dummy7, plot = TRUE)
+#' dummy6 <- data.frame(
+#' series = c("trs_30", "trs_31", "trs_32", "trs_34", "trs_35", "trs_36",
+#' "trs_37", "trs_38", "trs_39"),
+#' last = c(1000, 1009, 1007, 1005, 1010, 1020, 1025, 1050, 1035),
+#' n_sapwood = c(5, 10, 15, 16, 8, 0, 10, 3, 1),
+#' waneyedge = c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
+#' )
+#' tmp <- sw_sum(dummy6)
+#' sw_sum_plot(tmp, bar_col = "burlywood1")
 #'
 sw_sum_plot <- function(x, bar_col = "lightblue", spline_col = "tomato3") {
 
+        # to avoid notes in CMD check
+        spline <- SPD_wk <- year <- y <- SPD <- SPD_movAv <- NULL
+
+
 pdf_matrix <- as.data.frame(x)
-pdf_matrix <- subset(pdf_matrix, SPD > 0.0001 & !is.na(SPD) & !is.na(SPD_wk))
+# pdf_matrix <- subset(pdf_matrix, SPD > 0.0001 & !is.na(SPD)) & !is.na(SPD_wk))
 smooth <-
         as.data.frame(
                 movAv(pdf_matrix$SPD, 11, align = "center", edges = "fill")
@@ -32,17 +44,17 @@ plot_range <- range(pdf_matrix$year)
 p_max <- max(pdf_matrix$SPD)
 
 fd <- pdf_matrix[, c("year", "SPD_wk")]
-fd <- subset(fd, !is.na(SPD_wk))
-fd <- rep(fd$year, fd$SPD_wk)
-fd <- data.frame(fd)
-names(fd) <- "year"
-fd$y <- 1
-
-fd <- fd |>
+fd <- subset(fd, SPD_wk>0)
+if (nrow(fd) > 0){
+        fd <- rep(fd$year, fd$SPD_wk)
+        fd <- data.frame(fd)
+        names(fd) <- "year"
+        fd$y <- 1
+        fd <- fd |>
         dplyr::group_by(year) |>
         dplyr::mutate(y = cumsum(y)) |>
-        dplyr::left_join(pdf_matrix |>
-        dplyr::select("year", "SPD"), by = "year")
+        dplyr::left_join(pdf_matrix |> dplyr::select("year", "SPD"), by = "year")
+}
 
 p <-
      pdf_matrix |>
@@ -56,11 +68,11 @@ p <-
                            color = spline_col,
                            size = 1,
                            alpha = .5) +
-        ggplot2::geom_point(data = fd,
+        { if (nrow(fd) > 0) ggplot2::geom_point(data = fd,
                             ggplot2::aes(x = year, y = SPD + y * p_max/20),
                             size = 3,
                             shape = 8,
-                            colour = "navyblue") +
+                            colour = "navyblue") } +
         ggplot2::scale_x_continuous(
                 limits = c(plyr::round_any(plot_range[1], 10, floor),
                            plyr::round_any(plot_range[2], 10, ceiling)),
