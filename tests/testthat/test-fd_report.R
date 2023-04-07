a32508ae5ffb5bde960e30b4678548dd9b0bd699
Koen Van Daele <koen_van_daele@telenet.be>
2023-04-07 23:52:21 +0200

    Added more tests.

--- tests/testthat/test-fd_report.R
+++ tests/testthat/test-fd_report.R
@@ -15,17 +15,31 @@ testthat::test_that("Wrong arguments generate errors", {
      )
      testthat::expect_error(
           fd_report(testdata,
-               "series" = "id"
-          ),
-          regexp = "last"
-     )
-     testthat::expect_error(
-          fd_report(testdata,
                     "series" = "id",
                     "last"="end"
           ),
           regexp = "n_sapwood"
      )
+})
+
+testthat::test_that("last must be numeric", {
+     testthat::expect_error(
+          fd_report(testdata,
+                    "series" = "id"
+          ),
+          regexp = "last"
+     )
+     testdata <- data.frame(series = c("aaa", "bbb", "ccc"),
+                            n_sapwood = c(10, 11, 12),
+                            waneyedge = c(FALSE, FALSE,TRUE),
+                            last = c("honderddrieentwintig", 456, 1789))
+     testthat::expect_error(
+          fd_report(testdata),
+          regexp = "last"
+     )
+})
+
+testthat::test_that("waneyedge must be boolean", {
      testthat::expect_error(
           fd_report(testdata,
                     "series" = "id",
@@ -34,6 +48,48 @@ testthat::test_that("Wrong arguments generate errors", {
                     "waneyedge" = "waynesedge"
           ),
           regexp = "waneyedge"
+     )
+     testdata <- data.frame(id = c("aaa", "bbb", "ccc"),
+                            swr = c(10, 11, 12),
+                            waneyedge = c("ja", "nee", 345),
+                            end = c(123, 456, 1789))
+     testthat::expect_warning(
+          fd_report(testdata,
+                    "series" = "id",
+                    "last"="end",
+                    "n_sapwood"="swr",
+          ),
+          regexp = "waneyedge"
+     )
+})
+
+testthat::test_that("credMass must be between 0 and 1", {
+     testthat::expect_error(
+          fd_report(testdata,
+                    "series" = "id",
+                    "last" = "end",
+                    "n_sapwood" = "swr",
+                    "credMass" = "lots"
+          ),
+          regexp = "credMass"
+     )
+     testthat::expect_error(
+          fd_report(testdata,
+                    "series" = "id",
+                    "last" = "end",
+                    "n_sapwood" = "swr",
+                    "credMass" = -145
+          ),
+          regexp = "credMass"
+     )
+     testthat::expect_error(
+          fd_report(testdata,
+                    "series" = "id",
+                    "last" = "end",
+                    "n_sapwood" = "swr",
+                    "credMass" = 1.01
+          ),
+          regexp = "credMass"
      )
 })
 
