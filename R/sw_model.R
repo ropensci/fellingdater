78519ff1b243dd9ed6ae296b58e48b0b23aaba6c
Kristof Haneca <Kristof.Haneca@vlaanderen.be>
2025-06-05 14:29:47 +0200

    additional checks for sw_model and sw_model_plot

--- R/sw_model.R
+++ R/sw_model.R
@@ -12,14 +12,14 @@
 #'   * _lognormal_ (the default value),
 #'   * _normal_,
 #'   * _weibull_,
-#'   * _gammma_.
+#'   * _gamma_.
 #' @param cred_mass A `scalar` in the range of `[0, 1]` specifying the mass
 #'   within the credible interval (default = .954).
 #' @param plot A `logical`. If `TRUE` a plot of the fitted density function is
 #'   returned. When `FALSE`, a list with numeric output of the modelling process
 #'   is returned.
 #' @param source A character string with info on the data source. This info is
-#'   incorporated in the titel of the plot when a sapwood data set is provided
+#'   incorporated in the title of the plot when a sapwood data set is provided
 #'   from a user-defined `data.frame`.
 #'
 #' @return The return value depends on the `plot` parameter.
@@ -29,115 +29,119 @@
 #'
 #' @export
 #' @examples
-#' sw_model(sw_data = "Sohar_2012_ELL_c",
-#'           densfun = "lognormal",
-#'           cred_mass = .095,
-#'           plot = FALSE)
+#' sw_model(
+#'      sw_data = "Sohar_2012_ELL_c",
+#'      densfun = "lognormal",
+#'      cred_mass = .095,
+#'      plot = FALSE
+#' )
 #'
 #' sw_model("Hollstein_1980")
 #'
 sw_model <-
-  function(sw_data = "Hollstein_1980",
-           densfun = "lognormal",
-           cred_mass = 0.954,
-           source = NA,
-           plot = FALSE) {
-    if (is.na(cred_mass) || cred_mass <= 0 || cred_mass >= 1) {
-      stop("--> cred_mass must be between 0 and 1")
-    }
-    # to avoid notes in CMD check
-    count <- n_sapwood <- model_fit <- p <- NULL
+     function(sw_data = "Hollstein_1980",
+              densfun = "lognormal",
+              cred_mass = 0.954,
+              source = NA,
+              plot = FALSE) {
+          # to avoid notes in CMD check
+          count <- n_sapwood <- model_fit <- p <- NULL
 
-    check_densfun(densfun)
-    check_plot(plot)
-    check_cred_mass(cred_mass)
+          check_densfun(densfun)
+          check_plot(plot)
+          check_cred_mass(cred_mass)
 
-    if (!is.character(sw_data)) {
-      stop("--> sw_data should be one of `sw_data_overview()`
+
+          if (is.character(sw_data)) {
+               if (sw_data %in% sw_data_overview()) {
+                    observed <- get(sw_data)
+                    sw_source <- if (!is.na(source)) source else sw_data
+               } else {
+                    # user supplied data
+                    observed <- get(sw_data, envir = parent.frame())
+                    check_sapwood_data_user(observed)
+                    sw_source <- if (!is.na(source)) source else "user defined"
+               }
+          } else if (is.data.frame(sw_data)) {
+               observed <- sw_data
+               check_sapwood_data_user(observed)
+               sw_source <- if (!is.na(source)) source else "user defined"
+          } else {
+               stop("--> sw_data should be one of `sw_data_overview()`
 or the name a data.frame with numeric values in columns `n_sapwood` and `count`.)")
-    }
+          }
 
-    if (sw_data %in% sw_data_overview()) {
-      observed <- get(sw_data)
-      sw_source <- if (!is.na(source)) source else sw_data
-    }
-
-    if (!sw_data %in% sw_data_overview()) {
-      observed <- get(sw_data)
-      sw_source <- if (!is.na(source)) source else "user defined"
-    }
-
-    if (!all(c("n_sapwood", "count") %in% names(observed))) {
-      stop("--> sw_data should be a data.frame with numeric values in columns
+          if (!all(c("n_sapwood", "count") %in% names(observed))) {
+               stop("--> sw_data should be a data.frame with numeric values in columns
            `n_sapwood` and `count`.)")
-    }
+          }
 
-    if (!(is.numeric(observed$n_sapwood) && is.numeric(observed$count))) {
-      stop(
-        "--> sw_data should have numeric values in columns `n_sapwood` and `count`.)"
-      )
-    }
+          if (!(is.numeric(observed$n_sapwood) && is.numeric(observed$count))) {
+               stop(
+                    "--> sw_data should have numeric values in columns `n_sapwood` and `count`.)"
+               )
+          }
 
-    observed <- observed[, c("n_sapwood", "count")]
+          observed <- observed[, c("n_sapwood", "count")]
 
-    observed <- subset(observed, count > 0)
-    n_obs <- sum(observed$count)
-    min <- min(observed$n_sapwood)
-    max <- max(observed$n_sapwood)
-    mean <- round(mean(observed$n_sapwood), 2)
-    range <- c(min, mean, max)
-    names(range) <- c("min", "mean", "max")
+          observed <- subset(observed, count > 0)
+          n_obs <- sum(observed$count)
+          min <- min(observed$n_sapwood)
+          max <- max(observed$n_sapwood)
+          mean <- round(mean(observed$n_sapwood), 2)
+          range <- c(min, mean, max)
+          names(range) <- c("min", "mean", "max")
 
-    df <-
-      data.frame(n_sapwood = rep(observed$n_sapwood, observed$count))
+          df <-
+               data.frame(n_sapwood = rep(observed$n_sapwood, observed$count))
 
-    fit_params <-
-      MASS::fitdistr(df |> dplyr::pull(n_sapwood), densfun)
+          fit_params <-
+               MASS::fitdistr(df |> dplyr::pull(n_sapwood), densfun)
 
-    sw_model <- data.frame(
-      model_fit = d_dens(
-        densfun = densfun,
-        x = rep(1:100, 1),
-        param1 = fit_params$estimate[[1]],
-        param2 = fit_params$estimate[[2]],
-        n = n_obs
-      )
-    )
-    sw_model["n_sapwood"] <-
-      as.numeric(rownames(sw_model))
+          sw_model <- data.frame(
+               model_fit = d_dens(
+                    densfun = densfun,
+                    x = rep(1:100, 1),
+                    param1 = fit_params$estimate[[1]],
+                    param2 = fit_params$estimate[[2]],
+                    n = n_obs
+               )
+          )
+          sw_model["n_sapwood"] <-
+               as.numeric(rownames(sw_model))
 
-    sw_model <- sw_model |>
-      dplyr::mutate(p = model_fit / n_obs)
+          sw_model <- sw_model |>
+               dplyr::mutate(p = model_fit / n_obs)
 
-    sw_model <-
-      merge(sw_model, observed, all.x = TRUE) |>
-      dplyr::relocate(p, .after = n_sapwood)
+          sw_model <-
+               merge(sw_model, observed, all.x = TRUE) |>
+               dplyr::relocate(p, .after = n_sapwood)
 
-    hdi_model <-
-      hdi(
-        x = sw_model,
-        cred_mass = cred_mass
-      )
+          hdi_model <-
+               hdi(
+                    x = sw_model,
+                    cred_mass = cred_mass
+               )
 
-    spline_int <-
-      as.data.frame(stats::spline(
-        sw_model$n_sapwood,
-        sw_model$model_fit,
-        xout = seq(1, 100, 0.2)
-      ))
+          spline_int <-
+               as.data.frame(stats::spline(
+                    sw_model$n_sapwood,
+                    sw_model$model_fit,
+                    xout = seq(1, range[["max"]] + 10, 0.2)
+               ))
 
-    output <- list(
-      sapwood_data = sw_source,
-      n = n_obs,
-      range = range,
-      density_function = densfun,
-      fit_parameters = fit_params,
-      sapwood_model = sw_model,
-      hdi_model = hdi_model
-    )
-    if (plot == TRUE) {
-      sw_model_plot(output)
-    } else {
-      output
-    }
-  }
+          output <- list(
+               sapwood_data = sw_source,
+               n = n_obs,
+               range = range,
+               density_function = densfun,
+               fit_parameters = fit_params,
+               sapwood_model = sw_model,
+               hdi_model = hdi_model
+          )
+          if (isTRUE(plot)) {
+               sw_model_plot(output)
+          } else {
+               output
+          }
+     }
--- R/sw_model_plot.R
+++ R/sw_model_plot.R
@@ -11,119 +11,123 @@
 #' @return  A ggplot-style graph.
 #' @examples
 #' tmp <- sw_model(
-#'   sw_data = "Hollstein_1980",
-#'   densfun = "lognormal",
-#'   cred_mass = .95,
-#'   plot = FALSE
+#'      sw_data = "Hollstein_1980",
+#'      densfun = "lognormal",
+#'      cred_mass = .95,
+#'      plot = FALSE
 #' )
 #' sw_model_plot(tmp,
-#'               bar_fill = "forestgreen",
-#'               bar_color = "darkgreen",
-#'               line_color = "tomato3")
+#'      bar_fill = "forestgreen",
+#'      bar_color = "darkgreen",
+#'      line_color = "tomato3"
+#' )
 #'
 #' @export
 #'
 
 sw_model_plot <-
-        function(x,
-                 bar_fill = "steelblue3",
-                 bar_color = "grey60",
-                 line_color = "red3"
-                 ) {
-  if (!all(names(x) %in% c(
-    "sapwood_data",
-    "n",
-    "range",
-    "density_function",
-    "fit_parameters",
-    "sapwood_model",
-    "hdi_model"
-  ))
-  ) {
-    stop("Input differs from output sw_model()")
-  }
+     function(x,
+              bar_fill = "steelblue3",
+              bar_color = "grey60",
+              line_color = "red3") {
+          if (!all(names(x) %in% c(
+               "sapwood_data",
+               "n",
+               "range",
+               "density_function",
+               "fit_parameters",
+               "sapwood_model",
+               "hdi_model"
+          ))
+          ) {
+               stop("Input differs from output sw_model()")
+          }
 
-  # to avoid notes in CMD check
-  count <- n_sapwood <- y <- NULL
+          # to avoid notes in CMD check
+          count <- n_sapwood <- y <- NULL
 
-  max <- x$range[3]
-  hdi_lower <- x$hdi_model[[1]]
-  hdi_upper <- x$hdi_model[[2]]
-  spline_int <-
-    as.data.frame(stats::spline(
-      x$sapwood_model$n_sapwood,
-      x$sapwood_model$model_fit,
-      # xout = seq(1, max, 0.2)
-      xout = seq(1, 100, 0.2)
-    ))
-  spline_int <- subset(spline_int, y > 1e-02)
+          max <- x$range[3]
+          max_count <- max(x$sapwood_model$count, na.rm = TRUE)
+          hdi_lower <- x$hdi_model[[1]]
+          hdi_upper <- x$hdi_model[[2]]
+          spline_int <-
+               as.data.frame(stats::spline(
+                    x$sapwood_model$n_sapwood,
+                    x$sapwood_model$model_fit,
+                    # xout = seq(1, max, 0.2)
+                    xout = seq(1, x$range[["max"]] + 10, 0.2)
+               ))
 
-  p <- ggplot2::ggplot() +
-    ggplot2::geom_col(
-      data = x$sapwood_model,
-      ggplot2::aes(
-        x = n_sapwood,
-        y = count
-      ),
-      fill = bar_fill,
-      color = bar_color,
-      alpha = .4
-    ) +
-    ggplot2::geom_ribbon(
-      data = subset(
-        spline_int,
-        x >= hdi_lower &
-          x <= hdi_upper
-      ),
-      ggplot2::aes(
-        ymin = 0,
-        ymax = y,
-        x = x
-      ),
-      fill = "grey30",
-      alpha = 0.2
-    ) +
-    ggplot2::geom_segment(
-      ggplot2::aes(
-        x = hdi_lower,
-        xend = hdi_upper,
-        y = 0,
-        yend = 0
-      ),
-      colour = "grey30",
-      linewidth = .8,
-      alpha = 0.8
-    ) +
-    ggplot2::geom_line(
-      data = spline_int,
-      ggplot2::aes(x = x, y = y),
-      linewidth = 1,
-      color = line_color
-    ) +
-    ggplot2::scale_x_continuous(breaks = seq(0, 100, by = 10)) +
-    ggplot2::labs(
-      subtitle = paste0(
-        "sapwood data: ", x$sapwood_data, "<br>",
-        "<span style='color:", bar_fill, "'> original data</span> (n = ",
-        x$n,
-        ")",
-        "<br> <span style='color:", line_color,
-        "'>  fitted distribution</span> (",
-        x$density_function, ")", "<br> <span style='color:grey30'>hdi (",
-        round(x$hdi_model$p * 100, 1), "%)</span>: __between ",
-        hdi_lower,
-        " and ",
-        hdi_upper,
-        " sapwood rings__"
-      )
-    ) +
-    ggplot2::xlab("number of sapwood rings") +
-    ggplot2::ylab("n\n") +
-    theme_fdr() +
-    ggplot2::theme(
-      plot.subtitle = ggtext::element_markdown(hjust = 0),
-      panel.grid.major.y = ggplot2::element_line(),
-      panel.grid.minor.y = ggplot2::element_line()
-    )
-  suppressWarnings(print(p))
-}
+          # spline_int <- subset(spline_int, y > 1e-02)
+
+          plot <- ggplot2::ggplot() +
+               ggplot2::geom_col(
+                    # use filter to avoid Warnings
+                    data = dplyr::filter(x$sapwood_model, !is.na(count) & !is.na(n_sapwood)),
+                    ggplot2::aes(
+                         x = n_sapwood,
+                         y = count
+                    ),
+                    fill = bar_fill,
+                    color = bar_color,
+                    alpha = .4
+               ) +
+               ggplot2::geom_ribbon(
+                    data = subset(
+                         spline_int,
+                         x >= hdi_lower &
+                              x <= hdi_upper
+                    ),
+                    ggplot2::aes(
+                         ymin = 0,
+                         ymax = y,
+                         x = x
+                    ),
+                    fill = "grey30",
+                    alpha = 0.2
+               ) +
+               ggplot2::geom_segment(
+                    ggplot2::aes(
+                         x = hdi_lower,
+                         xend = hdi_upper,
+                         y = 0,
+                         yend = 0
+                    ),
+                    colour = "grey30",
+                    linewidth = .8,
+                    alpha = 0.8
+               ) +
+               ggplot2::geom_line(
+                    data = spline_int,
+                    ggplot2::aes(x = x, y = y),
+                    linewidth = 1,
+                    color = line_color
+               ) +
+               ggplot2::scale_x_continuous(breaks = seq(0, 100, by = 10)) +
+               ggplot2::scale_y_continuous(breaks = seq(0, max_count, by = if (max_count > 40) 10 else 5)) +
+               ggplot2::labs(
+                    subtitle = paste0(
+                         "sapwood data: ", x$sapwood_data, "<br>",
+                         "<span style='color:", bar_fill, "'> original data</span> (n = ",
+                         x$n,
+                         ")",
+                         "<br> <span style='color:", line_color,
+                         "'>  fitted distribution</span> (",
+                         x$density_function, ")", "<br> <span style='color:grey30'>hdi (",
+                         round(x$hdi_model$p * 100, 1), "%)</span>: between ",
+                         hdi_lower,
+                         " and ",
+                         hdi_upper,
+                         " sapwood rings"
+                    )
+               ) +
+               ggplot2::xlab("number of sapwood rings") +
+               ggplot2::ylab("n\n") +
+               theme_fdr() +
+               ggplot2::theme(
+                    plot.subtitle = ggtext::element_markdown(hjust = 0, lineheight = 1.2),
+                    panel.grid.major.y = ggplot2::element_line(),
+                    panel.grid.minor.y = ggplot2::element_line()
+               )
+          suppressWarnings(plot)
+     }
